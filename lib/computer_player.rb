caafcdb0e42da5b490c00ff5400a9f39ba1e8403
pumpkincouture <pumpkincouture@gmail.com>
2014-10-08 17:32:28 -0500

    Updating computer player

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -9,15 +9,18 @@ class ComputerPlayer
     @game_piece = X_PIECE
   end
 
-  def possible_moves(cells)
+  # def 
+
+  def possible_moves(board)
+
     move = []
-    cells.each_with_index do |subarray, idx|
+    board.cells.each_with_index do |subarray, idx|
       move << idx if subarray != @game_piece && subarray != O_PIECE
     end
-    move
+    move [ rand ]
   end
 
   def comp_move(move)
-    move[ rand(move.length) ]
+    move
   end 
 end
