5ec2ca8c6be46ae0a0fe232d9048c638a08897ab
pumpkincouture <pumpkincouture@gmail.com>
2014-10-09 15:52:33 -0500

    Randomized computer pick

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -8,13 +8,13 @@ class ComputerPlayer
   def initialize(game_piece)
     @game_piece = X_PIECE
   end
-
+  
   def possible_moves(board)
     move = []
     board.cells.each_with_index do |subarray, idx|
       move << idx if subarray != @game_piece && subarray != O_PIECE
     end
-    move [ rand ]
+    move [ rand(move.length) ]
   end
 
   def comp_move(move)
