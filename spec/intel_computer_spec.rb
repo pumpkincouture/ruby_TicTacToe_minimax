7a857203c3fb11593c760c104ceed95603ebe31b
pumpkincouture <pumpkincouture@gmail.com>
2014-10-14 21:29:54 -0500

    Updated minimax tests

--- spec/intel_computer_spec.rb
+++ spec/intel_computer_spec.rb
@@ -2,6 +2,7 @@ require_relative 'spec_helper.rb'
 
 describe IntelComputerPlayer do
 
+
 	before :each do
 	  @ai = IntelComputerPlayer.new(@game_piece)
 	  @ui = MockUi.new
@@ -10,74 +11,53 @@ describe IntelComputerPlayer do
 	end
 	
 	it "scores the game" do
-		@board.cells = ["O", [], "X", 
-		   			 				"X", [],  [], 
-		   			 				"X", "O","O"]
-		expect(@ai.get_score(@board)).to eq(0)
-	end
-
-	it "scores the game" do
-		@board.cells = ["O", [], "X", 
-		   			 				"X", "X",  [], 
+		@board.cells = ["O", "X", "X", 
+		   			 				"X", "X", "X", 
 		   			 				"X", "O","O"]
 		expect(@ai.get_score(@board)).to eq(1)
 	end
 
 	it "scores the game" do
-		@board.cells = ["O", [], "X", 
-		   			 				"X", [],  [], 
-		   			 				"X", "O","O"]
-		expect(@ai.get_score(@board)).to eq(0)
-	end
-
-	it "scores the game" do
-		@board.cells = ["O", [], "X", 
-		   			 				"X", "O",  [], 
+		@board.cells = ["O", "O", "O", 
+		   			 				"X", "X", "O", 
 		   			 				"X", "O","O"]
 		expect(@ai.get_score(@board)).to eq(-1)
 	end
 
-	it "returns move" do
+	it "scores the game" do
+		@board.cells = ["O", "X", "X", 
+		   			 				"X", "O", "O", 
+		   			 				"X", "O", "X"]
+		expect(@ai.get_score(@board)).to eq(0)
+	end
+
+	it "returns moves" do
 		@board.cells = ["O", [], "O", 
 		   			 				"X", "O", "X", 
 		   			 				"X", "O","X"]
-		expect(@ai.minimax(@board, 5, @ai.game_piece)).to eq(1)
+		expect(@ai.make_move(@board, 5, @ai.game_piece)).to eq(1)
 	end
 
-	it "returns move" do
+	it "returns moves" do
 		@board.cells = [ "X", [], "O", 
 		   			 				 [],  "O", "X", 
 		   			 				 "X", "O", "O"]
-		expect(@ai.minimax(@board, 5, @ai.game_piece)).to eq(3)
+		expect(@ai.make_move(@board, 5, @ai.game_piece)).to eq(3)
 	end
 
 
-	it "returns best move" do
+	it "returns moves" do
 		@board.cells = ["O", [], "X", 
 		   			 				"X", [], [], 
 		   			 				"X", "O","O"]
-		expect(@ai.minimax(@board, 5, @ai.game_piece)).to eq(4)
+		expect(@ai.make_move(@board, 5, @ai.game_piece)).to eq(4)
 	end
 
-	xit "returns best move" do
-		@board.cells = ["O", "O", [], 
-		   			 				"X", [], [], 
-		   			 				"X", "O","O"]
-		expect(@ai.minimax(@board, 5, @ai)).to eq(4)
-	end
-
-	xit "returns best move" do
-		@board.cells = ["X", "O", "O", 
-		   			 				"O", "X", [], 
-		   			 				"X", [], "O"]
-		expect(@ai.minimax(@board, 5, @ai)).to eq(5)
-	end
-
-	xit "returns best move" do
+	it "returns best move" do
 		@board.cells = ["O", "X", "O", 
-		   			 				"X", "O", [], 
+		   			 				[], [], [], 
 		   			 				[], [],[]]
-		expect(@ai.minimax(@board, 5, @ai)).to eq(1)
+		expect(@ai.make_move(@board, 5, @ai.game_piece)).to eq(4)
 	end
 
 end
