5c7b68960ff5ff64d1d66348d045e33f19944974
pumpkincouture <pumpkincouture@gmail.com>
2014-10-20 16:11:32 -0500

    Added game piece method

--- lib/player_factory.rb
+++ lib/player_factory.rb
@@ -4,13 +4,18 @@ require_relative 'ttt_constants'
 
 class PlayerFactory
 	include TTTConstants
-	
+
+	def get_game_piece
+		game_pieces = GAME_PIECES.values
+		@game_piece = game_pieces[rand(game_pieces.length)]
+	end
+
 	def create_player(choice)
 		case choice
 		  when EASY_PLAYER 
-		    ComputerPlayer.new(@game_piece)
+		    ComputerPlayer.new(get_game_piece)
 		  when AI_PLAYER
-		    IntelComputerPlayer.new(@game_piece)
+		    IntelComputerPlayer.new(get_game_piece)
 		  end
 	end
 end
