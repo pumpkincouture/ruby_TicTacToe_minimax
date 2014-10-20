d7bb32e1f83e845a1d724f4496517f30ad5385e7
pumpkincouture <pumpkincouture@gmail.com>
2014-10-20 16:16:09 -0500

    Updated game piece test

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -5,6 +5,7 @@ describe ComputerPlayer do
   let(:moves) { [0, 1, 2, 3, 4, 5, 6, 7, 8] }
   
 	before :each do
+		@game_piece = "&"
 	  @computer_player = ComputerPlayer.new(@game_piece)
 	  @ui = MockUi.new
 	  @choice = 3
@@ -12,7 +13,7 @@ describe ComputerPlayer do
 	end
 
 	it "identifies game piece" do
-		expect(@computer_player.game_piece).to eq("X")
+		expect(@computer_player.game_piece).to eq("&")
 	end
 
 	it "picks a move" do
