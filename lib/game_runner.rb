ff0fd71d3695eeb3841a78d76fff0fea910111fb
pumpkincouture <pumpkincouture@gmail.com>
2014-10-21 16:06:31 -0500

    Fixed logic

--- lib/game_runner.rb
+++ lib/game_runner.rb
@@ -1,7 +1,4 @@
-require_relative 'ttt_constants'
-
 class GameRunner
-  include TTTConstants
 
   attr_reader :board, :player, :human_player, :ui, :game_piece
     
@@ -17,8 +14,12 @@ class GameRunner
     until board.game_over?(human_player.game_piece, player.game_piece)
       play_game
     end
-    end_game_message(board.get_winning_player(board.check_matrix(human_player.game_piece, player.game_piece)))
+    end_game_message(get_board_winner)
   end
+
+  def get_board_winner
+    board.get_winning_player(board.check_matrix(human_player.game_piece, player.game_piece))
+  end    
 
   def first_move
     ui.welcome(player)
@@ -29,20 +30,20 @@ class GameRunner
   def play_game
     ui.user_prompt(human_player.game_piece)
     human_move = ui.get_user_answer
-      if board.invalid_key(human_move)
-        ui.user_error
-      else
-        board.valid_move(human_move, human_player.game_piece)
-        return true if board.winner?(human_player.game_piece, player.game_piece)
-        ui.computer_thinking
-        board.computer_move(player.comp_move(board), player.game_piece)
-      end
+    if board.invalid_key(human_move)
+      ui.user_error
+    else
+      board.valid_move(human_move, human_player.game_piece)
+      return true if board.winner?(human_player.game_piece, player.game_piece)
+      ui.computer_thinking
+      board.computer_move(player.comp_move(board), player.game_piece)
+    end
   end
 
   def end_game_message(winning_player)
-    if winning_player == @human_player.game_piece
+    if winning_player == human_player.game_piece
       ui.human_wins
-    elsif winning_player == @player.game_piece
+    elsif winning_player == player.game_piece
       ui.computer_wins
     else
       ui.cats_game
